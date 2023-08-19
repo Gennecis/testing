4215b1781e9e7596187d2a6966c22a60532b5318
Gennecis <okeziepascal2@gmail.com>
2023-08-18 19:04:50 -0700

    new addition to firstcode.py

--- firstcode.py
+++ firstcode.py
@@ -1,1 +1,7 @@
+amount_of_liquid = float(input("enter value L: "))
+amount_of_powder = float(input("enter value P: "))
+mixture_calibration = amount_of_liquid + amount_of_powder
+
+print("mixture caliberation = ", mixture_calibration)
+
 print("this is getting easier and more understanding for me")
